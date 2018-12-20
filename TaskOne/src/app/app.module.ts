29ff63fc26f6277f606d0dea020ca687265d3f6c
Jentoft <stefanjentoft@gmail.com>
2018-12-20 13:14:07 -0600

    Key for the table added

--- TaskOne/src/app/app.module.ts
+++ TaskOne/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './summary/summary.component';
 import { CurrentCasesComponent } from './current-cases/current-cases.component';
+import { CaseListComponent } from './case-list/case-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
     CurrentCasesComponent,
+    CaseListComponent,
   ],
   imports: [
     BrowserModule
